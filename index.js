0d8de4f85cdac55d09db00b897814565ea3461a9
JesusSerpaArrieta <jesus.serpa@cecar.edu.co>
2025-05-21 00:55:20 -0500

    commit test

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { sumar, restar, multiplicar, dividir } = require('./calculadora');
 
 // Probamos las funciones
 console.log('Sumar 5 + 3 =', sumar(5, 3));
+console.log('Sumar 5 + 3 =', sumar(5, 3));
 console.log('Restar 10 - 4 =', restar(10, 4));
 console.log('Multiplicar 6 * 7 =', multiplicar(6, 7));
 console.log('Dividir 20 / 5 =', dividir(20, 5));
